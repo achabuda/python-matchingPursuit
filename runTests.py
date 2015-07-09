106270076c9f85d2fb7e5d011b4750afb2ce468e
Tomasz Spustek <tomasz@spustek.pl>
2015-07-09 15:28:26 +0200

    unittests for dictionary features - work in progress

---
+++ runTests.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+'''
+#    This file is part of Matching Pursuit Python program (python-MP).
+#
+#    python-MP is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    python-MP is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with python-MP. If not, see <http://www.gnu.org/licenses/>.
+
+author: Tomasz Spustek
+e-mail: tomasz@spustek.pl
+University of Warsaw, July 06, 2015
+'''
+
+import unittest
+from tests.testDictionary import DictionaryTest
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
---
+++ tests/testDictionary.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+'''
+#    This file is part of Matching Pursuit Python program (python-MP).
+#
+#    python-MP is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    python-MP is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with python-MP. If not, see <http://www.gnu.org/licenses/>.
+
+author: Tomasz Spustek
+e-mail: tomasz@spustek.pl
+University of Warsaw, July 06, 2015
+'''
+
+import unittest
+from src.dictionary import gaussEnvelope
+
+def fun(x):
+	return x + 1
+
+class DictionaryTest(unittest.TestCase):
+	def test_1(self):
+		self.assertEqual(fun(3), 5)
+
+	def test_2(self):
+		self.assertEqual(fun(3), 4)
